783acf59b72ece3ef5dd49eb93a022b5d7aa1d4e
AlanRB08 <alanreyesbueno08@gmail.com>
2026-05-20 16:21:38 -0600

    chore: scaffold project and configure core dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+     <main className="min-h-screen bg-slate-950 text-white grid place-items-center">
+      <h1 className="text-4xl font-bold">Memory Game Challenge - Alan Reyes</h1>
+    </main>
+    </>
+  )
+}
+
+export default App
